3a613ccad4623e7fd8959d6d023d3466d84a71d7
neverfolding99-sudo <neverfolding99@gmail.com>
2026-08-26 11:46:06 +0200

    Change p.post to app.post for login route

--- server.js
+++ server.js
@@ -248,7 +248,7 @@ setInterval(async () => {
     }
 }, 20000);
 
-p.post('/api/login', (req, res) => {
+app.post('/api/login', (req, res) => {
   const { password } = req.body;
   if (password === ADMIN_PASSWORD) {
     return res.json({ ok: true });
